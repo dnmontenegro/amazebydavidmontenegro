1eb879815cf697c625bf83b887f15411f3635a1b
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-27 03:19:59 -0400

    Added necessary button functionality to advance for PlayManuallyActivity

--- app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/PlayManuallyActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/PlayManuallyActivity.java
@@ -2,10 +2,14 @@ package edu.wm.cs.cs301.DavidMontenegro.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.wm.cs.cs301.DavidMontenegro.R;
 
@@ -14,6 +18,15 @@ public class PlayManuallyActivity extends AppCompatActivity {
     private Button wholeMaze;
     private Button solution;
     private Button visibleWalls;
+    private Button shortcut;
+    private Button up;
+    private Button left;
+    private Button jump;
+    private Button right;
+    private Button down;
+    private Button manBack;
+    private int energy;
+    private int path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +36,37 @@ public class PlayManuallyActivity extends AppCompatActivity {
         wholeMaze = (Button) findViewById(R.id.wholeMaze);
         solution = (Button) findViewById(R.id.solution);
         visibleWalls = (Button) findViewById(R.id.visibleWalls);
+        shortcut = (Button) findViewById(R.id.shortcut);
+        up = (Button) findViewById(R.id.up);
+        left = (Button) findViewById(R.id.left);
+        jump = (Button) findViewById(R.id.jump);
+        right = (Button) findViewById(R.id.right);
+        down = (Button) findViewById(R.id.down);
+        manBack = (Button) findViewById(R.id.manBack);
+        energy = 0;
+        path = 0;
+
+        shortcut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FinishActivity.class);
+                i.putExtra("EnergyConsumption", energy);
+                i.putExtra("PathLength", path);
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.shortcut), getString(R.string.inputDetected));
+                startActivity(i);
+            }
+        });
+
+        manBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), AMazeActivity.class);
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.manBack), getString(R.string.inputDetected));
+                startActivity(i);
+            }
+        });
 
 
     }
