1cf843e4ad191f967313ece5fd44947626bc11c4
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-26 21:24:53 -0400

    Added working seekbar to AMazeActivity

--- app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/AMazeActivity.java
@@ -3,10 +3,12 @@ package edu.wm.cs.cs301.DavidMontenegro.gui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import edu.wm.cs.cs301.DavidMontenegro.R;
 
@@ -15,6 +17,8 @@ public class AMazeActivity extends AppCompatActivity {
     private TextView title;
     private TextView tSize;
     private SeekBar size;
+    private TextView tProgress;
+    private int mazeSize;
     private TextView tRooms;
     private Button rooms;
     private TextView tAlgorithm;
@@ -31,15 +35,31 @@ public class AMazeActivity extends AppCompatActivity {
 
         title = (TextView) findViewById(R.id.title);
         tSize = (TextView) findViewById(R.id.tSize);
+        size = (SeekBar) findViewById(R.id.size);
+        tProgress = (TextView) findViewById(R.id.tProgress);
         tRooms = (TextView) findViewById(R.id.tRooms);
         tAlgorithm = (TextView) findViewById(R.id.tAlgorithm);
         tMode = (TextView) findViewById(R.id.tMode);
 
-        title.setText(R.string.title);
-        tSize.setText(R.string.tSize);
-        tRooms.setText(R.string.tRooms);
-        tAlgorithm.setText(R.string.tAlgorithm);
-        tMode.setText(R.string.tMode);
+        size.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                seekBar.setMax(15);
+                tProgress.setText(getString(R.string.size, progress));
+                mazeSize = progress;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v((String) getApplicationContext().getText(R.string.tSize), (String) getApplicationContext().getText(R.string.inputDetected));
+            }
+        });
 
     }
 }
