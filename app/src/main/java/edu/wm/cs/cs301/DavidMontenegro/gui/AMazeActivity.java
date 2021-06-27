643a37adb1e7b8a0a690a29ff6ed37b3676bbb2b
David Montenegro <dnmontenegro@email.wm.edu>
2021-06-27 00:01:24 -0400

    Added spinners and buttons to AMazeActivity

--- app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/AMazeActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/AMazeActivity.java
@@ -2,9 +2,12 @@ package edu.wm.cs.cs301.DavidMontenegro.gui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
@@ -27,8 +30,10 @@ public class AMazeActivity extends AppCompatActivity {
     private boolean mazeRooms;
     private TextView tAlgorithm;
     private Spinner algorithm;
+    private String mazeAlgorithm;
     private TextView tMode;
     private Spinner mode;
+    private String mazeMode;
     private Button revisit;
     private Button explore;
 
@@ -46,7 +51,13 @@ public class AMazeActivity extends AppCompatActivity {
         rooms = (ToggleButton) findViewById(R.id.rooms);
         mazeRooms = true;
         tAlgorithm = (TextView) findViewById(R.id.tAlgorithm);
+        algorithm = (Spinner) findViewById(R.id.algorithm);
+        mazeAlgorithm = getString(R.string.DFS);
         tMode = (TextView) findViewById(R.id.tMode);
+        mode = (Spinner) findViewById(R.id.mode);
+        mazeMode = getString(R.string.Manual);
+        revisit = (Button) findViewById(R.id.revisit);
+        explore = (Button) findViewById(R.id.explore);
 
         size.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -64,7 +75,7 @@ public class AMazeActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
                 Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
-                Log.v((String) getApplicationContext().getText(R.string.tSize), (String) getApplicationContext().getText(R.string.inputDetected));
+                Log.v(getString(R.string.tSize), Integer.toString(mazeSize));
             }
         });
 
@@ -74,15 +85,85 @@ public class AMazeActivity extends AppCompatActivity {
                 if(isChecked) {
                     mazeRooms = true;
                     Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
-                    Log.v((String) getApplicationContext().getText(R.string.tRooms), (String) getApplicationContext().getText(R.string.inputDetected));
+                    Log.v(getString(R.string.tRooms), Boolean.toString(mazeRooms));
                 }
                 else {
                     mazeRooms = false;
                     Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
-                    Log.v((String) getApplicationContext().getText(R.string.tRooms), (String) getApplicationContext().getText(R.string.inputDetected));
+                    Log.v(getString(R.string.tRooms), Boolean.toString(mazeRooms));
                 }
             }
         });
+
+        String [] algorithms = {getString(R.string.DFS), getString(R.string.Prim), getString(R.string.Kruskal)};
+        ArrayAdapter algor = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, algorithms);
+        algor.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        algorithm.setAdapter(algor);
+        algorithm.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mazeAlgorithm = algorithm.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.tAlgorithm), mazeAlgorithm);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        String [] modes = {getString(R.string.Manual), getString(R.string.WallFollower), getString(R.string.Wizard)};
+        ArrayAdapter mod = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_spinner_item, modes);
+        mod.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mode.setAdapter(mod);
+        mode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                mazeMode = mode.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.tMode), mazeMode);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        revisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GeneratingActivity.class);
+                i.putExtra("MazeSize", mazeSize);
+                i.putExtra("MazeRooms", mazeRooms);
+                i.putExtra("MazeAlgorithm", mazeAlgorithm);
+                i.putExtra("MazeMode", mazeMode);
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.revisit), getString(R.string.inputDetected));
+                startActivity(i);
+            }
+        });
+
+        explore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), GeneratingActivity.class);
+                i.putExtra("MazeSize", mazeSize);
+                i.putExtra("MazeRooms", mazeRooms);
+                i.putExtra("MazeAlgorithm", mazeAlgorithm);
+                i.putExtra("MazeMode", mazeMode);
+                Toast.makeText(getApplicationContext(), R.string.inputDetected, Toast.LENGTH_SHORT).show();
+                Log.v(getString(R.string.explore), getString(R.string.inputDetected));
+                startActivity(i);
+            }
+        });
+
+
+
+
+
+
 
     }
 }
