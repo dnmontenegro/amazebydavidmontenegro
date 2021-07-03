16f07cf20455f8c6443466d23f1fa30682d07b9d
David Montenegro <dnmontenegro@email.wm.edu>
2021-07-03 19:36:44 -0400

    Added MazePanel.java and P5Panel.java

---
+++ app/src/main/java/edu/wm/cs/cs301/DavidMontenegro/gui/MazePanel.java
@@ -0,0 +1,13 @@
+package edu.wm.cs.cs301.DavidMontenegro.gui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MazePanel extends View   {
+
+	public MazePanel(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
